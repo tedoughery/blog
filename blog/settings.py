03fdbd88ee769dc36e25d61c5ad427d8a148921f
T. Eric. Doughery <32188051+tedoughery@users.noreply.github.com>
2023-04-17 17:14:33 -0500

    basic blog design

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blogApp.apps.BlogappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'TD',
+        'title': 'Test 1',
+        'content': 'Hello World',
+        'date_posted': 'April 17, 2023'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h2>Blog Home</h2>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blogApp/home.html', context)
 
 def about(request):
-    return HttpResponse('<h2>About</h2>')
+    return render(request, 'blogApp/about.html', {'title':'About'})
