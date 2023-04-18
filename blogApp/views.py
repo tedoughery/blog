72a6f23c95833c17310f466a6fd272451b7f5f21
T. Eric. Doughery <32188051+tedoughery@users.noreply.github.com>
2023-04-18 14:48:28 -0500

    made migrations, created admin

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'TD',
-        'title': 'Test 1',
-        'content': 'Hello World',
-        'date_posted': 'April 17, 2023'
-    }
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blogApp/home.html', context)
 
